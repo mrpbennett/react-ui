0e430abd4946a37d0851e0957dc306f4fb21e8dd
Paul Bennett <pbennett.uk@outlook.com>
2020-07-01 19:02:34 +0100

    build

---
+++ build/precache-manifest.4316524e1b5cb3718056bc65bfe15d1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "502f765ab8325be4a335b49fb17fe3fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc54b867c4aa5846f508",
+    "url": "/static/css/main.0d79a7b7.chunk.css"
+  },
+  {
+    "revision": "b4c014abeee47f2c96c3",
+    "url": "/static/js/2.d98fc3a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d98fc3a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc54b867c4aa5846f508",
+    "url": "/static/js/main.82df794a.chunk.js"
+  },
+  {
+    "revision": "041621c0757848d23643",
+    "url": "/static/js/runtime-main.89ce72de.js"
+  }
+]);
